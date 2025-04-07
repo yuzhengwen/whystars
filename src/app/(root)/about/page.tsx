1a034cc1305dd6d1f2e6b96af487b386fee55535
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-08 02:16:27 +0800

    Added content to home and about pages

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,12 +1,48 @@
-import { getAllUsers } from "@/actions/timetable";
-
 export default async function Home() {
-  const users = await getAllUsers(); // Fetch user timetables for user with ID 1
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
-      <p>{JSON.stringify(users)}</p>
+    <div className="flex flex-col items-center justify-center w-full ">
+      <div className="flex flex-col items-start justify-center w-2xl p-4 m-5">
+        <h1 className="text-3xl">
+          What can <span className="font-bold">Why Stars</span> do?
+        </h1>
+        <ul className="mt-4 text-lg list-disc list-inside">
+          <li>Search for modules and their respective index</li>
+          <li>Easy visual layout and configuring of timetable</li>
+          <li>Generate valid timetables based on the selected modules</li>
+          <li>Save the generated timetable</li>
+          <li>Access your timetable anywhere by creating an account</li>
+        </ul>
+        <h2 className="mt-4 text-2xl">Upcoming Features</h2>
+        <ul className="mt-4 text-lg list-disc list-inside">
+          <li>AI Integration</li>
+          <li>Save & load multiple timetables</li>
+          <li>Much more...</li>
+        </ul>
+        <h1 className="mt-4 text-3xl">Source Code</h1>
+        <p className="text-blue-700 mt-4 text-lg hover:text-blue-600 hover:underline cursor-pointer">
+          <a
+            href="https://github.com/yuzhengwen/whystars"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Repo
+          </a>
+        </p>
+        <p className="mt-2 text-m">
+          This is a one developer project, if you would like to contribute, feel
+          free to fork the repo and create a pull request. I would love to see
+          your contributions! If you have any suggestions or feedback, please
+          feel free to create an issue on GitHub.
+        </p>
+        <h2 className="mt-4 text-2xl">Stack</h2>
+        <ul className="mt-4 text-m list-disc list-inside">
+          <li>Framework: Next.js 15</li>
+          <li>Styling: Tailwind CSS & Shadcn</li>
+          <li>Database: Neon, Prisma ORM</li>
+          <li>Auth: Auth.js</li>
+          <li>Deployment: Vercel</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl">Hello Next</h1>
-      <Button className="p-5">
-        Hi
-      </Button>
-    </>
+    <div className="flex flex-col items-center justify-center w-full ">
+      <div className="flex flex-col items-center justify-center w-2xl p-4 m-5">
+        <h1 className="text-3xl">Still a WIP</h1>
+        <p className="mt-4 text-lg">
+          Feel free to try some of the features. Or take a look at the About
+          page for more information.
+        </p>
+      </div>
+    </div>
   );
 }
