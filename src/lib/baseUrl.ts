5cc88fce9e31fe97807e260c29e07b6058c95b7b
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-08 01:51:37 +0800

    use window.location.origin

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,4 +1,1 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-    : process.env.LOCAL_BASE_URL;
+export const baseUrl = window.location.origin;
