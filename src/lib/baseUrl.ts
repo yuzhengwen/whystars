11189564cc9a42880879b6f0ee9b102ac76f49a1
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-08 01:17:54 +0800

    try vercelurl

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,2 +1,4 @@
 export const baseUrl =
-  process.env.NODE_ENV === "production" ? "" : process.env.LOCAL_BASE_URL;
+  process.env.NODE_ENV === "production"
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.LOCAL_BASE_URL;
