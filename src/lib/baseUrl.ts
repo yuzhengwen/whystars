f0cc357be362996b592660adf1247f4ce4880c8c
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-08 01:44:25 +0800

    update NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     : process.env.LOCAL_BASE_URL;
