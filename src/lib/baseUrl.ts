0cc773187ca390b1115a27fc3a09179944362fe4
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-09 20:06:39 +0800

    Update to use production url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,8 +1,8 @@
 export const baseUrl: string =
   process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
       : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
       : ""
     : "http://localhost:3000";
