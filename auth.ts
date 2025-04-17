1a79ad11dd48d685f7c880baf0c9dd3ddd671b27
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-04-17 16:21:50 +0800

    Added Google Auth

--- auth.ts
+++ auth.ts
@@ -2,8 +2,9 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import GitHub from "next-auth/providers/github"
 import { prisma } from "@/lib/prisma"
+import Google from "next-auth/providers/google"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  providers: [GitHub, Google],
 })
