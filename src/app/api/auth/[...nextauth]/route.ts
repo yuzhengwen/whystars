e0a6532ce9b97a752214cd225353b44e98968b8c
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-11-22 22:39:20 +0800

    added dynamic params

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import { handlers } from "@/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
+
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
+export const dynamicParams = true; // Add this
