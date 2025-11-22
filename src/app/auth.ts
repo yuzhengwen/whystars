07e87a4b8179025792c29d2ef28bfd34e5003526
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-11-22 15:38:12 +0800

    added trust host in auth

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -7,4 +7,5 @@ import Google from "next-auth/providers/google"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
+  trustHost: true,
 })
