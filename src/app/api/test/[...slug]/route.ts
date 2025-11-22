68e85712a67a3e7f4e6180edaf62ea738eca8176
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-11-22 22:28:32 +0800

    fixed test route

--- src/app/api/test/[...slug]/route.ts
+++ src/app/api/test/[...slug]/route.ts
@@ -1,8 +1,9 @@
 // app/api/test/[...slug]/route.ts
 export async function GET(
   request: Request,
-  { params }: { params: { slug: string[] } }
+  props: { params: Promise<{ slug: string[] }> }
 ) {
+  const params = await props.params;
   return Response.json({ 
     message: "Catch-all works!",
     slug: params.slug 
